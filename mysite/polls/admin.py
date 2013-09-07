0c6ba8c31677119cd361bf32493ec1b971564878
German Larrain <glarrain@users.noreply.github.com>
2013-09-07 15:33:38 -0500

    polls.admin: added 'PollAdmin'

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -4,4 +4,17 @@ from polls.models import Choice, Poll
 
 
 admin.site.register(Choice)
-admin.site.register(Poll)
+
+
+class PollAdmin(admin.ModelAdmin):
+
+    fieldsets = [
+        (None,               {'fields': ['question']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    list_display = ('question', 'pub_date')
+    list_filter = ['pub_date']
+    search_fields = ['question']
+    date_hierarchy = 'pub_date'
+
+admin.site.register(Poll, PollAdmin)
