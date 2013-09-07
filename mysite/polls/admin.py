c2fe269858dedea1cc4eb8cc842ca9cb1189f050
German Larrain <glarrain@users.noreply.github.com>
2013-09-07 15:37:06 -0500

    polls.admin: added 'ChoiceInline'

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -3,6 +3,11 @@ from django.contrib import admin
 from polls.models import Choice, Poll
 
 
+class ChoiceInline(admin.TabularInline):
+
+    model = Choice
+    extra = 3
+
 admin.site.register(Choice)
 
 
@@ -12,6 +17,7 @@ class PollAdmin(admin.ModelAdmin):
         (None,               {'fields': ['question']}),
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
+    inlines = [ChoiceInline]
     list_display = ('question', 'pub_date')
     list_filter = ['pub_date']
     search_fields = ['question']
