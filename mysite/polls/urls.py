b36e933023449cfa294b5b024236e8a6281da4f7
German Larrain <glarrain@users.noreply.github.com>
2013-09-07 23:55:40 -0500

    changed URLs configuration, reversing and hardcoding: -root urls: set namespace 'polls' for pattern "/polls/" -polls.templates: replaced hardcoded urls with template tag 'url' -polls.urls: set name for all patterns; referenced view functions directly, not as strings

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,15 +1,16 @@
 from django.conf.urls import patterns, url
 from django.views.generic import DetailView, ListView
 
+from polls import views
 from polls.models import Poll
 
 
 # urlpatterns = patterns(
-#     'polls.views',
-#     url(r'^$', 'index'),
-#     url(r'^(?P<poll_id>\d+)/$', 'detail'),
-#     url(r'^(?P<poll_id>\d+)/results/$', 'results'),
-#     url(r'^(?P<poll_id>\d+)/vote/$', 'vote'),
+#     '',
+#     url(r'^$', views.index, name='index'),
+#     url(r'^(?P<poll_id>\d+)/$', views.detail, name='detail'),
+#     url(r'^(?P<poll_id>\d+)/results/$', views.results, name='results'),
+#     url(r'^(?P<poll_id>\d+)/vote/$', views.vote, name='vote'),
 # )
 
 urlpatterns = patterns(
@@ -18,15 +19,17 @@ urlpatterns = patterns(
         ListView.as_view(
             queryset=Poll.objects.order_by('-pub_date')[:5],
             context_object_name='latest_poll_list',
-            template_name='polls/index.html')),
+            template_name='polls/index.html'),
+        name='index'),
     url(r'^(?P<pk>\d+)/$',
         DetailView.as_view(
             model=Poll,
-            template_name='polls/detail.html')),
+            template_name='polls/detail.html'),
+        name='detail'),
     url(r'^(?P<pk>\d+)/results/$',
         DetailView.as_view(
             model=Poll,
             template_name='polls/results.html'),
-        name='poll_results'),
-    url(r'^(?P<poll_id>\d+)/vote/$', 'polls.views.vote'),
+        name='results'),
+    url(r'^(?P<poll_id>\d+)/vote/$', views.vote, name='vote'),
 )
